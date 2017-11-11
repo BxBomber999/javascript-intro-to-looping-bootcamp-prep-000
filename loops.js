972d0f8e70fb05863d31bec36738d5ff2644dd24
Peter Coyle <coyle.peters@gmail.com>
2017-11-11 22:13:25 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,19 +1,6 @@
 function forLoop(arr){
-  for (let i = 0; i < 25; i++) {
-     arr.push(`I am ${i} strange for loops.`)
+  for(i=0;i<25;i++){
+    arr.push(`I am ${i} strange for loop.`)
   }
   return arr
-}
-
-function whileLoop(n){
-  while(n>=0){
-    console.log(n)
-    n--
-  }
-}
-
-function doWhileLoop(array){
-  do {
-    array.pop()
-  } while (array.length > 0 && maybeTrue())
 }
